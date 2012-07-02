4c94ccd7a55b17584c8a14132564bf5c6d58feb1
antonio <antonio@Antonio-PC.Home>
2012-07-02 23:51:32 +0200

    Creadas todas las entidades,

--- src/Cupon/OfertaBundle/Entity/Oferta.php
+++ src/Cupon/OfertaBundle/Entity/Oferta.php
@@ -1,7 +1,391 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+namespace Cupon\OfertaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cupon\OfertaBundle\Entity\Oferta
+ *
+ * @ORM\Table()
+ * @ORM\Entity
  */
-?>
+class Oferta
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $nombre
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string $slug
+     *
+     * @ORM\Column(name="slug", type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @var text $descripcion
+     *
+     * @ORM\Column(name="descripcion", type="text")
+     */
+    private $descripcion;
+
+    /**
+     * @var string $foto
+     *
+     * @ORM\Column(name="foto", type="string", length=255)
+     */
+    private $foto;
+
+    /**
+     * @var decimal $precio
+     *
+     * @ORM\Column(name="precio", type="decimal")
+     */
+    private $precio;
+
+    /**
+     * @var decimal $descuento
+     *
+     * @ORM\Column(name="descuento", type="decimal")
+     */
+    private $descuento;
+
+    /**
+     * @var datetime $fecha_publicacion
+     *
+     * @ORM\Column(name="fecha_publicacion", type="datetime")
+     */
+    private $fecha_publicacion;
+
+    /**
+     * @var datetime $fecha_expiracion
+     *
+     * @ORM\Column(name="fecha_expiracion", type="datetime")
+     */
+    private $fecha_expiracion;
+
+    /**
+     * @var integer $compras
+     *
+     * @ORM\Column(name="compras", type="integer")
+     */
+    private $compras;
+
+    /**
+     * @var integer $umbral
+     *
+     * @ORM\Column(name="umbral", type="integer")
+     */
+    private $umbral;
+
+    /**
+     * @var boolean $revisada
+     *
+     * @ORM\Column(name="revisada", type="boolean")
+     */
+    private $revisada;
+
+    /**
+     * @var string $ciudad
+     *
+     * @ORM\Column(name="ciudad", type="string", length=255)
+     */
+    private $ciudad;
+
+    /**
+     * @var string $tienda
+     *
+     * @ORM\Column(name="tienda", type="string", length=255)
+     */
+    private $tienda;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param text $descripcion
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return text 
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set foto
+     *
+     * @param string $foto
+     */
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+    }
+
+    /**
+     * Get foto
+     *
+     * @return string 
+     */
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    /**
+     * Set precio
+     *
+     * @param decimal $precio
+     */
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+
+    /**
+     * Get precio
+     *
+     * @return decimal 
+     */
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set descuento
+     *
+     * @param decimal $descuento
+     */
+    public function setDescuento($descuento)
+    {
+        $this->descuento = $descuento;
+    }
+
+    /**
+     * Get descuento
+     *
+     * @return decimal 
+     */
+    public function getDescuento()
+    {
+        return $this->descuento;
+    }
+
+    /**
+     * Set fecha_publicacion
+     *
+     * @param datetime $fechaPublicacion
+     */
+    public function setFechaPublicacion($fechaPublicacion)
+    {
+        $this->fecha_publicacion = $fechaPublicacion;
+    }
+
+    /**
+     * Get fecha_publicacion
+     *
+     * @return datetime 
+     */
+    public function getFechaPublicacion()
+    {
+        return $this->fecha_publicacion;
+    }
+
+    /**
+     * Set fecha_expiracion
+     *
+     * @param datetime $fechaExpiracion
+     */
+    public function setFechaExpiracion($fechaExpiracion)
+    {
+        $this->fecha_expiracion = $fechaExpiracion;
+    }
+
+    /**
+     * Get fecha_expiracion
+     *
+     * @return datetime 
+     */
+    public function getFechaExpiracion()
+    {
+        return $this->fecha_expiracion;
+    }
+
+    /**
+     * Set compras
+     *
+     * @param integer $compras
+     */
+    public function setCompras($compras)
+    {
+        $this->compras = $compras;
+    }
+
+    /**
+     * Get compras
+     *
+     * @return integer 
+     */
+    public function getCompras()
+    {
+        return $this->compras;
+    }
+
+    /**
+     * Set umbral
+     *
+     * @param integer $umbral
+     */
+    public function setUmbral($umbral)
+    {
+        $this->umbral = $umbral;
+    }
+
+    /**
+     * Get umbral
+     *
+     * @return integer 
+     */
+    public function getUmbral()
+    {
+        return $this->umbral;
+    }
+
+    /**
+     * Set revisada
+     *
+     * @param boolean $revisada
+     */
+    public function setRevisada($revisada)
+    {
+        $this->revisada = $revisada;
+    }
+
+    /**
+     * Get revisada
+     *
+     * @return boolean 
+     */
+    public function getRevisada()
+    {
+        return $this->revisada;
+    }
+
+    /**
+     * Set ciudad
+     *
+     * @param string $ciudad
+     */
+    public function setCiudad($ciudad)
+    {
+        $this->ciudad = $ciudad;
+    }
+
+    /**
+     * Get ciudad
+     *
+     * @return string 
+     */
+    public function getCiudad()
+    {
+        return $this->ciudad;
+    }
+
+    /**
+     * Set tienda
+     *
+     * @param string $tienda
+     */
+    public function setTienda($tienda)
+    {
+        $this->tienda = $tienda;
+    }
+
+    /**
+     * Get tienda
+     *
+     * @return string 
+     */
+    public function getTienda()
+    {
+        return $this->tienda;
+    }
+    
+    public function __toString()
+    {
+        return $this->getNombre();
+    }
+
+}
